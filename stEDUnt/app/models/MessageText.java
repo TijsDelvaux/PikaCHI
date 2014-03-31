bbc05171cf2fa7fda07333f952a03b0215b75401
TijsDelvaux <tijs.delvaux@gmail.com>
2014-03-31 14:41:34 +0200

    hat

--- stEDUnt/app/models/MessageText.java
+++ stEDUnt/app/models/MessageText.java
@@ -1,5 +1,16 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.*;
+import play.db.ebean.*;
+import com.avaje.ebean.*;
+
+@Entity
 public class MessageText {
 
+	@Id
+	long id;
+	
+	String text;
 }
