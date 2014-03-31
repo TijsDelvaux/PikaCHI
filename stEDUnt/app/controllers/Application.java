1333e067625755c6379f1f93436672c04e6ddaea
Serge Hendrickx <sergehendrickx@gmail.com>
2014-03-31 13:51:35 +0200

    Begin of student and login

--- stEDUnt/app/controllers/Application.java
+++ stEDUnt/app/controllers/Application.java
@@ -3,12 +3,49 @@ package controllers;
 import play.*;
 import play.mvc.*;
 
+import play.data.*; 
+import static play.data.Form.*;
+
 import views.html.*;
 
 public class Application extends Controller {
 
     public static Result index() {
         return ok(index.render("Your new application is ready."));
+    }
+    
+    
+    public static Result login() {
+        return ok(
+            login.render(Form.form(Login.class))
+        );
+    }
+    
+    public static class Login {
+
+        public String email;
+        public String password;
+        
+        public String validate() {
+            if (Student.authenticate(email, password) == null) {
+              return "Invalid user or password";
+            }
+            return null;
+        }
+        
+        public static Result authenticate() {
+            Form<Login> loginForm = Form.form(Login.class).bindFromRequest();
+            if (loginForm.hasErrors()) {
+                return badRequest(login.render(loginForm));
+            } else {
+                session().clear();
+                session("email", loginForm.get().email);
+                return redirect(
+                    routes.Application.index()
+                );
+            }
+        }
+
     }
 
 }
--- stEDUnt/app/models/Student.java
+++ stEDUnt/app/models/Student.java
@@ -2,16 +2,44 @@ package models;
 
 import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.*;
+import play.db.ebean.*;
+import com.avaje.ebean.*;
 
-public class Student {
+@Entity
+public class Student extends Model{
 	
 	private StudentAdvertisement studentAdv;
 	private TutorAdvertisement tutorAdv;
 	private List<Message> messages;
 	
-	public Student(){
+    @Id
+    public String email;
+    public String name;
+    public String password;
+    public Language language;
+    
+	public Student(String email, String name, String password){
 		studentAdv = null;
 		tutorAdv = null;
 		messages = new ArrayList<Message>();
 	}
+	
+	public static Finder<String,Student> find = new Finder<String,Student>(
+	        String.class, Student.class
+	    ); 
+	
+	public static Student authenticate(String email, String password) {
+        return find.where().eq("email", email)
+            .eq("password", password).findUnique();
+    }
+	
+	public static Language getLanguage(){
+		return language;
+	}
+	
+
+
+	
+	
 }
