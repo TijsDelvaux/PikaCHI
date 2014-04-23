2a86cb2a8147094b4a7f008d63cf251c2a3679a5
Ine <Ine@10.43.19.194>
2014-04-23 14:48:47 +0200

    mijn advertenties (begin) naar taal omgezet

--- stEDUnt/app/models/Language.java
+++ stEDUnt/app/models/Language.java
@@ -7,9 +7,12 @@ public enum Language {
 		public String toString() {return "Nederlands";}
 		public String myTutorAdvertisements() {return "Mijn bijles-advertenties";}
 		public String messages() {return "Berichten";}
-		public String viewAdvertisements(){return "Bekijk berichten";}
-		
-		
+		public String viewAdvertisements(){return "Bekijk advertenties";}
+		public String myAdvertisements(){return "Mijn advertenties";}
+		public String postAdvertisement(){return "Advertentie posten: ";}
+		public String searchTutor(){return "Bijles zoeken";}
+		public String giveTutoring(){return "Bijles geven";}
+		public String changeAdvertisements(){return "Advertentie's bekijken of aanpassen: ";}
 	}, 
 	
 	English{
@@ -18,6 +21,11 @@ public enum Language {
 		public String myTutorAdvertisements() {return null;}
 		public String messages() {return null;}
 		public String viewAdvertisements(){return null;}
+		public String myAdvertisements(){return null;}
+		public String postAdvertisement(){return null;}
+		public String searchTutor(){return null;}
+		public String giveTutoring(){return null;}
+		public String changeAdvertisements(){return null;}
 		
 	};
 	
@@ -25,5 +33,9 @@ public enum Language {
 	public abstract String myTutorAdvertisements();
 	public abstract String messages();
 	public abstract String viewAdvertisements();
-	
+	public abstract String myAdvertisements();
+	public abstract String postAdvertisement();
+	public abstract String searchTutor();
+	public abstract String giveTutoring();
+	public abstract String changeAdvertisements();
 }
